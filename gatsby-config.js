dbe1cc3aec7f8b88daa6c549e7d096091b6d4e00
Larisa Bainton <larisabainton@gmail.com>
2018-11-23 11:05:23 -0500

    personalize configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,34 @@
 module.exports = {
     siteMetadata: {
-        title: 'Larisa Bainton Website',
-        description: 'Sample',
-        content: 'Sample',
-        name: 'Sample Name',
+        title: 'LARISA',
+        description: 'Larisa Bainton',
+        content: 'Larisa',
+        name: 'Larisa Bainton',
         links: [
-            { name: 'facebook', url: 'https://www.facebook.com' },
-            { name: 'twitter', url: 'https://www.twitter.com' },
-            { name: 'instagram', url: 'https://www.instagram.com' },
-            { name: 'linkedIn', url: 'https://www.linkedin.com' },
+            { name: 'facebook', url: 'https://www.facebook.com/motherlarisa' },
+            { name: 'instagram', url: 'https://www.instagram.com/mother_larisa/' },
+            { name: 'linkedIn', url: 'https://www.linkedin.com/in/larisabainton/' },
         ],
         sections: [
-            { name: 'intro', title: 'Hey', text: 'Hey this is the intro' },
-            { name: 'gallery', title: 'Gallery', text: 'Here is some text about what I do' },
-            { name: 'contact', title: 'Say Hello.', text: 'Keep in touch' },
+            { name: 'intro', title: 'Hi', text: 'I\'m Larisa. Scroll down to learn more about me' },
+            {
+                name: 'gallery', headerTitle: 'Videos', title: 'Videos', videos: '<iframe title="Videos" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PLWI_rNoeUtePh_Ul-icIPUCu6z67UUd3v" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />',
+            },
+            {
+                name: 'contact', headerTitle: 'Contact', title: 'Say Hello', text: "Let's talk about music, software, or anything really.",
+            },
         ],
         descriptions: [
-            { headerTitle: 'About Me', title: 'Section 1 Title', text: 'This is the text for section 1' },
-            { headerTitle: 'What I Do', title: 'Section 2 Title', text: 'This is the text for section 2' },
+            {
+                headerTitle: 'Musician',
+                title: 'I\'m a singer',
+                text: "I'm a classical singer who enjoys experimenting with other styles. You'll find me performing in operas, jazz ensembles, and contemporary improvisation ensembles around Boston.",
+            },
+            {
+                headerTitle: 'Developer',
+                title: 'I\'m a software engineer',
+                text: 'JavaScript is my specialty. Check out some of my work here soon.',
+            },
         ],
     },
     plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-sass', 'gatsby-plugin-eslint'],
